e4df16a17f2a83ed646aef5712250afb26188f3b
MeishaPD <putradewanm@student.ub.ac.id>
2024-12-14 16:35:19 +0700

    feat(backend): creating documents/note. connected to Convex and Clerk

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,10 +4,27 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
 
     const { user } = useUser();
+
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({
+            title: "Untitled Document",
+        });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "Note created successfully!",
+            error: "Failed to create a new note.",
+        });
+    };
 
     return ( 
         <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -28,7 +45,7 @@ const DocumentsPage = () => {
             <h2 className="text-lg font-medium">
                 Welcome to the Documents Page! {user?.username}&apos;s documents will be displayed here.
             </h2>
-            <Button>
+            <Button onClick={onCreate}>
                 <PlusCircle className="h-6 w-6 mr-2" />
                 Create a note
             </Button>
